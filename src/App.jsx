3bf22dc547a933b0282c68538a8beddcf83c8e94
Swamy <swamysatish2244@gmail.com>
2024-03-17 17:04:30 +0530

    portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
 import React from 'react'
 import Header from './Components/header/header.jsx'
-import About from './Components/About/About.jsx'
+import About from './Components/About/about.jsx'
 import Contact from './Components/Contact/Contact.jsx'
 import Experiance from './Components/Experiance/Experiance.jsx'
 import Skills from './Components/Skills/Skills.jsx'
 import Footer from './Components/Footer/Footer.jsx'
 import Services from './Components/Services/Services.jsx'
+import './App.css'
 
 
 const App = () => {
   return (
-    <div>
+    <div className='App'>
       <Header/>
       <About/>
-      <Contact/>
       <Experiance/>
       <Skills/>
-      <Footer/>
       <Services/>
+      <Contact/>
+      <Footer/>
     </div>
   )
 }
--- src/Components/header/header.jsx
+++ src/Components/header/header.jsx
@@ -9,9 +9,9 @@ const Header = () => {
   return (
     <header> 
       <div className="container header_container">
-        <h5>HelloI'm</h5>
+        <h5>Hello, I'm</h5>
         <h1>Swamy</h1>
-        <h5 className="text-light">Full Stack Developer</h5>
+        <h5 className="text-light">Fullstack Developer</h5>
         <CTA />
         <HeaderSocials />
         <Nav />
